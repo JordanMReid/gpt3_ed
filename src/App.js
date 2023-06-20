bf6f6967cad22c170a865ad4183ea2d9e6c152b8
JordanMReid <108962349+JordanMReid@users.noreply.github.com>
2023-06-19 22:38:05 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {  Blog, Possibility, Features, WhatGPT3, Header, Footer } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../Assets/assets/gpt3-assets/google.png';
+import slack from '../../Assets/assets/gpt3-assets/slack.png';
+import atlassian from '../../Assets/assets/gpt3-assets/atlassian.png';
+import dropbox from '../../Assets/assets/gpt3-assets//dropbox.png';
+import shopify from '../../Assets/assets/gpt3-assets/shopify.png';
+
+export {
+  google,
+  slack,
+  atlassian,
+  dropbox,
+  shopify,
+};
